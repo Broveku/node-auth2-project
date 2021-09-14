c17c385d934bd23ee446f12686fdd688f65a20b4
Broveku <gearhearjordan@gmail.com>
2021-09-14 14:47:56 -0700

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,20 +2,24 @@ const bcrypt = require('bcryptjs')
 const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // use this secret!
-const Users = require('../../api/users/users-model')
+const User = require('../users/users-model')
 
 router.post("/register", validateRoleName, (req, res, next) => {
-  let user = req.body
-  const rounds = process.env.BCRYPT_ROUNDS || 8
-  const hash = bcrypt.hashSync(user.password, rounds)
+  const { username, password } = req.body
+  const { role_name } = req
+  const hash = bcrypt.hashSync(password, 8)
 
-  user.password = hash
-
-  Users.add(user)
+  User.add({username, password: hash, role_name})
     .then(newUser =>{
-      res.status(201).json({newUser})
-  })
+      res.status(201).json({
+        user: newUser.user,
+        username: newUser.username,
+        role_name: newUser.role_name
+      })
+    })
     .catch(next)
+
+
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
 
@@ -31,7 +35,7 @@ router.post("/register", validateRoleName, (req, res, next) => {
 
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
-  let 
+  
 
   /**
     [POST] /api/auth/login { "username": "sue", "password": "1234" }
