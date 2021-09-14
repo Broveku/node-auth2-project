803b167e8da02156c4ba950056fc63e5ce25f770
Broveku <gearhearjordan@gmail.com>
2021-09-14 15:15:59 -0700

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,23 +1,22 @@
 const jwt = require('jsonwebtoken')
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 const Users = require('../../api/users/users-model')
+const { findBy } = require('../users/users-model')
 
 const restricted = (req, res, next) => {
-  const token = req.headers.authorization
-    if (!token) return next({
-      status: 401, message: 'Token required'
-    })
-    jwt.verify(
-      token,
-      JWT_SECRET,
-      (err, decoded) =>{
-        if (err) return next({
-          status:401, message: 'Token invalid', realErrorMessage: err.message,
-        })
-        req.decodedJwt = decoded
-        next()
-      }
-    )
+  
+ const token = req.headers.authorization
+ if (!token){
+   return next({status:401, message:'Token required'})
+ }
+ jwt.verify(token, JWT_SECRET, (err, decodedToken)=>{
+  if (err){
+    next({ status:401, message: 'Token invalid'})
+  } else{
+    req.decodedToken = decodedToken
+    next()
+  }
+ })
 
   /*
     If the user does not provide a token in the Authorization header:
@@ -60,16 +59,17 @@ const only = role_name => (req, res, next) => {
 
 
 const checkUsernameExists = async (req, res, next) => {
-  try {
-    const users = await Users.findBy({ username: req.body.username })
-    if (!users){
-      next({
-        status: 401, message: 'Invalid credentials'
-      })
+  try{
+    const [user] = await findBy({username: req.body.username})
+    if (!user){
+      next({status:422, message: 'Invalid credentials'})
+    } else {
+      req.user = user
+      next()
     }
-  }catch(err){
-      next(err)
-    }
+  } catch (err) {
+    next(err)
+  }
   /*
     If the username in req.body does NOT exist in the database
     status 401
